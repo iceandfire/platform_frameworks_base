c52b218c7a37b2957734e8ed622164631d2e727b
Manoj Kumar AVM <manojavm@codeaurora.org>
2013-12-30 22:35:22 -0800

    Update WFD session orientation based on UIBC rotation
    User Input Back Channel (UIBC) support in Wifi Display stack
    allows the sink to send control/commands/preferences to the source
    e.g. touch, keyboard/mouse entry, or orientation/rotation.
    
    PhoneWindowManager needs to listen for UIBC rotation
    intents and update the preferred rotation when WFD is active.
    
    Note: HDMI rotation lock overrides WiFi Display UIBC rotation.
    
    Change-Id: I1869b21c3c03b5b36c5381933b50dfbfeca88db6

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -584,7 +584,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int MSG_LAUNCH_VOICE_ASSIST_WITH_WAKE_LOCK = 12;
     private static final int MSG_POWER_DELAYED_PRESS = 13;
     private static final int MSG_POWER_LONG_PRESS = 14;
-    boolean mWifiDisplayConnected;
+    boolean mWifiDisplayConnected = false;
+    int     mWifiDisplayCustomRotation = -1;
 
     private class PolicyHandler extends Handler {
         @Override
@@ -5025,6 +5026,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 } else {
                     mWifiDisplayConnected = false;
                 }
+                mWifiDisplayCustomRotation =
+                        intent.getIntExtra("wfd_UIBC_rot", -1);
                 updateRotation(true);
             }
         }
@@ -5365,6 +5368,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // Ignore sensor when plugged into HDMI when demo HDMI rotation lock enabled.
                 // Note that the dock orientation overrides the HDMI orientation.
                 preferredRotation = mDemoHdmiRotation;
+            } else if ( mWifiDisplayConnected && (mWifiDisplayCustomRotation > -1)) {
+                // Ignore sensor when WFD is active and UIBC rotation is enabled
+                preferredRotation = mWifiDisplayCustomRotation;
             } else if (mHdmiPlugged && mDockMode == Intent.EXTRA_DOCK_STATE_UNDOCKED
                     && mUndockedHdmiRotation >= 0) {
                 // Ignore sensor when plugged into HDMI and an undocked orientation has
